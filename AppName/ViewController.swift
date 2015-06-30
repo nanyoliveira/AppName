2560364deb46ee627b8857d13fd7a453179cca1c
Ariane Oliveira <arianeoliveira@Arianes-MBP.home>
2015-06-30 00:01:47 -0300

    Initial Commit

---
+++ AppName/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppName
+//
+//  Created by Ariane Oliveira on 30/06/2015.
+//  Copyright (c) 2015 Ariane Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
